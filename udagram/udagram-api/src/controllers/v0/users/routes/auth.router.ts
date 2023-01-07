df1aa4fb0cb8fca6dd437c53b5ecf5ca2b83e80e
Amaniatiah <amaniatiah@gmail.com>
2023-01-08 01:19:13 +0300

    first commit

--- udagram/udagram-api/src/controllers/v0/users/routes/auth.router.ts
+++ udagram/udagram-api/src/controllers/v0/users/routes/auth.router.ts
@@ -7,7 +7,7 @@ import * as c from '../../../../config/config';
 import * as jwt from 'jsonwebtoken';
 import {NextFunction} from 'connect';
 
-// import * as EmailValidator from 'email-validator';
+import * as EmailValidator from 'email-validator';
 import {config} from 'bluebird';
 
 const router: Router = Router();
@@ -56,7 +56,7 @@ router.post('/login', async (req: Request, res: Response) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  if (!email) {
+  if (!email || !EmailValidator.validate(email)) {
     return res.status(400).send({auth: false, message: 'Email is required or malformed.'});
   }
 
@@ -84,7 +84,7 @@ router.post('/', async (req: Request, res: Response) => {
   const email = req.body.email;
   const plainTextPassword = req.body.password;
 
-  if (!email ) {
+  if (!email || !EmailValidator.validate(email)) {
     return res.status(400).send({auth: false, message: 'Email is missing or malformed.'});
   }
 
